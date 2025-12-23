0076449bba900a157cc6ec6d9278b6c70fe8bbe5
ST4RL1GHT <dryuakita@gmail.com>
2025-12-23 22:25:37 +0900

    Stripping-whitespaces

---
+++ Stripping-whitespace.py
@@ -0,0 +1,10 @@
+# Stripping whitespace
+greet = '   I like learning python programming.   '
+greet = greet.lstrip()
+print(greet.lstrip())
+
+greet = greet.rstrip()
+print(greet.rstrip())
+
+greet = greet.strip()
+print(greet.strip())
